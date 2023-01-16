4c74ae19454b9cf85efa1133fb350be03b7d3ef8
Siahei Audzeichyk <siarhei.audzeichyk@gmail.com>
2023-01-16 19:32:34 +0100

    Implemented main function

--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -14,10 +14,29 @@ def create_parser():
         """)
 
     parser.add_argument("url", help="URL of the database to backup")
-    parser.add_argument("--driver",
+    parser.add_argument("--driver", "-d",
         help="How & where to store the backup",
         nargs=2,
+        metavar=("DRIVER", "DESTINATION"),
         action=DriverAction,
         required=True)
 
     return parser
+
+def main():
+    import boto3
+    import time
+    from pgbackup import pgdump, storage
+
+    args = create_parser().parse_args()
+    dump = pgdump.dump(args.url)
+    if args.driver == 's3':
+        client = boto3.client('s3')
+        timestamp = time.strftime("%Y-%m-%dT%H%M", time.localtime())
+        file_name = pgdump.dump_file_name(args.url, timestamp)
+        print(f"Backing database up to {args.destination} in S3 as {file_name}")
+        storage.s3(client, dump.stdout, args.destination, file_name)
+    else:
+        outfile = open(args.destination, 'wb')
+        print(f"Backing database up locally to {outfile.name}")
+        storage.local(dump.stdout, outfile)
